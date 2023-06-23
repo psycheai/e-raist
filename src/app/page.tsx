dc0cab23f36b6c834d1e175d416690d04a9544d7
Mukul108 <Mukulpal108@hotmail.com>
2023-06-23 22:18:32 +0530

    css cng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from "next/image";
 import schoolLogo from "../../public/logo.png";
 import backgroundImg from "../../public/mar.jpg";
-
+import styles from './globals.css';
 
 const HomePage = () => {
   return (
@@ -18,35 +18,34 @@ const HomePage = () => {
           />
         </div>
         <div className="relative z-10 flex flex-col items-center justify-center text-white mt-8">
-         <div className="bg-white bg-opacity-20 p-6 sm:p-24 flex flex-col sm:flex-row items-center justify-center rounded-2xl mt-8 container-margin">
+        <div className={`box ${styles.contentSquare} ml-4`}>
             <div className="mb-8 sm:mb-0 sm:mr-8 container-size">
               <Image
                 src={schoolLogo}
                 alt="School Logo"
-                width={400}
-                height={400}
+                width={150}
+                height={150}
               />
             </div>
             <div className="container-size">
-              <h1 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
-                GOLDEN
+              <h1 className="h1">
+                WELCOME TO OUR SCHOOLS 
               </h1>
-              <h2 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
-                JUBILEE
+              <h2 className="h2">
+                50TH ANNIVERSARY E-MAGAZINE
               </h2>
               
               <div className="flex flex-col items-center">
-                <hr className="w-2/3 sm:w-full border border-golden my-4" />
-                <h3>
+                
+                <h3 className="h3">
                 Celebrating 50 Years of Excellence, Memories, and Success
                 </h3>
-                <hr className="w-2/3 sm:w-full border border-golden my-4" />
+               
               </div>
             </div>
+           
           </div>
         </div>
-        
-
       </div>
     </div>
   );
